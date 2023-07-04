1b64da663712d1e779bcb773d13720e19ca633ab
CodeWithPrateekk <pru151998@gmail.com>
2023-07-04 19:53:19 +0530

    assign

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -15,6 +15,8 @@ import java.util.UUID;
 @RequestMapping("/hotel")
 public class HotelManagementController {
 
+    HotelManagementService hotelManagementService = new HotelManagementService();
+
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
 
@@ -24,7 +26,7 @@ public class HotelManagementController {
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
 
 
-        return HotelManagementService.addHotel(hotel);
+        return hotelManagementService.addHotel(hotel);
     }
 
     @PostMapping("/add-user")
@@ -33,7 +35,7 @@ public class HotelManagementController {
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
 
-       return HotelManagementService.addUser(user);
+       return hotelManagementService.addUser(user);
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -43,7 +45,7 @@ public class HotelManagementController {
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
 
-        return HotelManagementService.getHotelWithMostFacilities();
+        return hotelManagementService.getHotelWithMostFacilities();
     }
 
     @PostMapping("/book-a-room")
@@ -56,14 +58,14 @@ public class HotelManagementController {
         //If there arent enough rooms available in the hotel that we are trying to book return -1 
         //in other case return total amount paid 
         
-        return HotelManagementService.bookARoom(booking);
+        return hotelManagementService.bookARoom(booking);
     }
     
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person 
-        return HotelManagementService.getBookings(aadharCard);
+        return hotelManagementService.getBookings(aadharCard);
     }
 
     @PutMapping("/update-facilities")
@@ -73,7 +75,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        return HotelManagementService.updateFacilities(newFacilities,hotelName);
+        return hotelManagementService.updateFacilities(newFacilities,hotelName);
     }
 
 }
--- src/main/java/com/driver/controllers/HotelManagementService.java
+++ src/main/java/com/driver/controllers/HotelManagementService.java
@@ -8,29 +8,31 @@ import com.driver.model.User;
 import java.util.List;
 
 public class HotelManagementService {
-    public static String addHotel(Hotel hotel) {
 
-        return HotelManagementRepository.addHotel(hotel);
+    HotelManagementRepository hotelManagementRepository = new HotelManagementRepository();
+    public String addHotel(Hotel hotel) {
+
+        return hotelManagementRepository.addHotel(hotel);
     }
 
-    public static Integer addUser(User user) {
+    public Integer addUser(User user) {
 
-        return HotelManagementRepository.addUser(user);
+        return hotelManagementRepository.addUser(user);
     }
 
-    public static String getHotelWithMostFacilities() {
-        return HotelManagementRepository.getHotelWithMostFacilities();
+    public String getHotelWithMostFacilities() {
+        return hotelManagementRepository.getHotelWithMostFacilities();
     }
 
-    public static int bookARoom(Booking booking) {
-        return HotelManagementRepository.bookARoom(booking);
+    public int bookARoom(Booking booking) {
+        return hotelManagementRepository.bookARoom(booking);
     }
 
-    public static Hotel updateFacilities(List<Facility> newFacilities, String hotelName) {
-        return HotelManagementRepository.updateFacilities(newFacilities,hotelName);
+    public Hotel updateFacilities(List<Facility> newFacilities, String hotelName) {
+        return hotelManagementRepository.updateFacilities(newFacilities,hotelName);
     }
 
-    public static int getBookings(Integer aadharCard) {
-        return HotelManagementRepository.getBookings(aadharCard);
+    public int getBookings(Integer aadharCard) {
+        return hotelManagementRepository.getBookings(aadharCard);
     }
 }
